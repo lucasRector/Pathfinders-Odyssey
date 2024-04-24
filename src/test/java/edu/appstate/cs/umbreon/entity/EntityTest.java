049d03b792c9da0b9f71b5c32b5bcb8e79d06d4b
Ahtheboy <ahtheboy@pobox.com>
2024-04-24 17:02:08 -0400

    This adds a test stub for the Entity class and it adds Mockito as a dependency.

---
+++ src/test/java/edu/appstate/cs/umbreon/entity/EntityTest.java
@@ -0,0 +1,26 @@
+package edu.appstate.cs.umbreon.entity;
+
+import static org.mockito.Mockito.mock;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import edu.appstate.cs.umbreon.main.GamePanel;
+
+public class EntityTest
+{
+    @Test
+    public void testDraw0()
+    {
+        //GamePanel gp = new GamePanel();
+        //Entity entity = new Entity(gp);
+
+        System.out.println("Test ran!");
+        assertTrue(true);
+
+    }
+
+    // TODO: Create other tests like this.
+}
